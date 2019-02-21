9b6d255f5259dca7ed1c44582f267c2de1639ca6
Jacky (Jie) Liu <jackyjieliu@gmail.com>
2019-02-21 18:59:00 +0800

    page

---
+++ precache-manifest.fa9192aa17697ee49f893e080b6857f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c49919c216f9e7ac03b127d6ffba19ad",
+    "url": "/static/media/logo.c49919c2.jpg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "2197933185635cd74932",
+    "url": "/static/js/main.21979331.chunk.js"
+  },
+  {
+    "revision": "2ff1e1234599ef72db92",
+    "url": "/static/js/2.2ff1e123.chunk.js"
+  },
+  {
+    "revision": "2197933185635cd74932",
+    "url": "/static/css/main.87987fad.chunk.css"
+  },
+  {
+    "revision": "3745322e3a8ffba5d5f3d75ba4347b09",
+    "url": "/index.html"
+  }
+];
