20e58a66d0220a7ae4aa8addab0a0f4c91b34fa7
Jacky (Jie) Liu <jackyjieliu@gmail.com>
2019-02-21 19:02:11 +0800

    page

---
+++ precache-manifest.50376a1f98ac7f608e305eddab53e53b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c49919c216f9e7ac03b127d6ffba19ad",
+    "url": "/find-fifth-order/static/media/logo.c49919c2.jpg"
+  },
+  {
+    "revision": "09bb607adc855d8e0fd4",
+    "url": "/find-fifth-order/static/js/runtime~main.09bb607a.js"
+  },
+  {
+    "revision": "eea4f73d8dfb3b819385",
+    "url": "/find-fifth-order/static/js/main.eea4f73d.chunk.js"
+  },
+  {
+    "revision": "7f43886dba3ed0096766",
+    "url": "/find-fifth-order/static/js/2.7f43886d.chunk.js"
+  },
+  {
+    "revision": "eea4f73d8dfb3b819385",
+    "url": "/find-fifth-order/static/css/main.87987fad.chunk.css"
+  },
+  {
+    "revision": "d559ba74a93756c1473e56c17a063932",
+    "url": "/find-fifth-order/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fa9192aa17697ee49f893e080b6857f3.js"
+  "/find-fifth-order/precache-manifest.50376a1f98ac7f608e305eddab53e53b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/find-fifth-order/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
